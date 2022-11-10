90f33b05e5a1ddffcb43a637e6339814cb89d9a9
Tom Roar Furunes <tom.roar@halloverden.no>
2022-11-10 12:00:01 +0100

    Initial bundle setup

---
+++ src/HalloVerdenAzureServiceBusMessengerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace HalloVerden\AzureServiceBusMessengerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HalloVerdenAzureServiceBusMessengerBundle extends Bundle {
+
+}

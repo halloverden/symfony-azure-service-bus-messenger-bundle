3d9d12677e42f43ed0a940101217af0c721d121d
Tom Roar Furunes <tom.roar@halloverden.no>
2022-11-14 15:28:10 +0100

    ADD: AzureServiceBusReceivedStamp

---
+++ src/Transport/AzureServiceBusReceivedStamp.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace HalloVerden\AzureServiceBusMessengerBundle\Transport;
+
+use HalloVerden\AzureServiceBusMessengerBundle\Azure\ServiceBus\BrokerProperties;
+use HalloVerden\AzureServiceBusMessengerBundle\Azure\ServiceBus\CustomProperties;
+use Symfony\Component\Messenger\Stamp\NonSendableStampInterface;
+
+class AzureServiceBusReceivedStamp implements NonSendableStampInterface {
+  private BrokerProperties $brokerProperties;
+  private CustomProperties $customProperties;
+
+  /**
+   * AzureServiceBusReceivedStamp constructor.
+   */
+  public function __construct(BrokerProperties $brokerProperties, CustomProperties $customProperties) {
+    $this->brokerProperties = $brokerProperties;
+    $this->customProperties = $customProperties;
+  }
+
+  /**
+   * @return BrokerProperties
+   */
+  public function getBrokerProperties(): BrokerProperties {
+    return $this->brokerProperties;
+  }
+
+  /**
+   * @return CustomProperties
+   */
+  public function getCustomProperties(): CustomProperties {
+    return $this->customProperties;
+  }
+
+}

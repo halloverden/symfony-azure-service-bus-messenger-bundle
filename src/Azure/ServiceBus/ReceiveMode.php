6fe854adede7fa3788373bf414291a3d265f511a
Tom Roar Furunes <tom.roar@halloverden.no>
2022-11-14 15:27:45 +0100

    ADD: ReceiveMode

---
+++ src/Azure/ServiceBus/ReceiveMode.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace HalloVerden\AzureServiceBusMessengerBundle\Azure\ServiceBus;
+
+use Symfony\Component\HttpFoundation\Request;
+
+enum ReceiveMode {
+  case RECEIVE_AND_DELETE;
+  case PEEK_LOCK;
+
+  /**
+   * @return string
+   */
+  public function getMethod(): string {
+    return match ($this) {
+      self::RECEIVE_AND_DELETE => Request::METHOD_DELETE,
+      self::PEEK_LOCK => Request::METHOD_POST,
+    };
+  }
+}
